9658c52d0b133e814d084a8eead17719344c9a3b
JitteryProgrammer <120346064+JitteryProgrammer@users.noreply.github.com>
2023-06-30 00:24:06 -0300

    Update ShieldAWS.js

--- ShieldAWS.js
+++ ShieldAWS.js
@@ -33,71 +33,83 @@ client.on('message', async (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  // Verificar o comando 'verificar'
-  if (command === 'verificar') {
-    // Verificar os argumentos do comando
-    if (args.length !== 1) {
-      return message.reply('Você precisa fornecer o nome da instância para realizar a verificação!');
+  try {
+    // Tratamento de erros mais robusto
+    switch (command) {
+      case 'verificar':
+        await verificarCommand(args, message);
+        break;
+      // Adicione outros comandos aqui
+      default:
+        message.reply('Comando inválido!');
+        break;
     }
-
-    try {
-      // Obter o nome da instância
-      const instanceName = args[0];
-
-      // Definir os parâmetros da instância
-      const params = {
-        Filters: [
-          {
-            Name: 'tag:Name',
-            Values: [instanceName]
-          }
-        ]
-      };
-
-      // Verificar se a instância existe
-      const data = await ec2.describeInstances(params).promise();
-
-      // Verificar se a instância existe
-      if (data.Reservations.length === 0) {
-        return message.reply(`Nenhuma instância com o nome '${instanceName}' foi encontrada!`);
-      }
-
-      // Verificar todas as instâncias
-      for (const reservation of data.Reservations) {
-        for (const instance of reservation.Instances) {
-          // Verificar se a instância possui regras de firewall
-          if (instance.SecurityGroups.length === 0) {
-            return message.reply(`A instância '${instanceName}' não possui regras de firewall ativas!`);
-          }
-
-          // Verificar todas as regras de firewall da instância
-          for (const securityGroup of instance.SecurityGroups) {
-            // Verificar todas as regras da instância
-            const rulesParams = {
-              GroupIds: [securityGroup.GroupId]
-            };
-
-            const sgData = await ec2.describeSecurityGroups(rulesParams).promise();
-
-            // Verificar todas as regras da instância
-            for (const rule of sgData.SecurityGroups[0].IpPermissions) {
-              // Verificar o tipo da regra
-              const type = rule.IpProtocol === '-1' ? 'All' : rule.IpProtocol;
-              // Verificar a porta aberta
-              const port = rule.FromPort === -1 ? 'All' : rule.FromPort;
-              // Verificar quem está autorizado a se conectar
-              const source = rule.IpRanges.length > 0 ? rule.IpRanges[0].CidrIp : 'All';
-              // Avisar o usuário
-              message.reply(`Regra '${type}' encontrada na porta '${port}' para '${source}'`);
-            }
-          }
-        }
-      }
-    } catch (error) {
-      return message.reply(`Erro ao verificar a segurança da instância '${args[0]}': ${error.message}`);
-    }
+  } catch (error) {
+    message.reply(`Erro ao executar o comando '${command}': ${error.message}`);
+    console.error(error);
   }
 });
+
+// Comando 'verificar'
+async function verificarCommand(args, message) {
+  // Verificar os argumentos do comando
+  if (args.length !== 1) {
+    return message.reply('Você precisa fornecer o nome da instância para realizar a verificação!');
+  }
+
+  // Obter o nome da instância
+  const instanceName = args[0];
+
+  // Definir os parâmetros da instância
+  const params = {
+    Filters: [
+      {
+        Name: 'tag:Name',
+        Values: [instanceName]
+      }
+    ]
+  };
+
+  // Verificar se a instância existe
+  const data = await ec2.describeInstances(params).promise();
+
+  // Verificar se a instância existe
+  if (data.Reservations.length === 0) {
+    return message.reply(`Nenhuma instância com o nome '${instanceName}' foi encontrada!`);
+  }
+
+  // Verificar todas as instâncias
+  for (const reservation of data.Reservations) {
+    for (const instance of reservation.Instances) {
+      // Verificar se a instância possui regras de firewall
+      if (instance.SecurityGroups.length === 0) {
+        return message.reply(`A instância '${instanceName}' não possui regras de firewall ativas!`);
+      }
+
+      // Verificar todas as regras de firewall da instância
+      for (const securityGroup of instance.SecurityGroups) {
+        // Verificar todas as regras da instância
+        const rulesParams = {
+          GroupIds: [securityGroup.GroupId]
+        };
+
+        const sgData = await ec2.describeSecurityGroups(rulesParams).promise();
+
+        // Verificar todas as regras da instância
+        for (const rule of sgData.SecurityGroups[0].IpPermissions) {
+          // Verificar o tipo da regra
+          const type = rule.IpProtocol === '-1' ? 'All' : rule.IpProtocol;
+          // Verificar a porta aberta
+          const port = rule.FromPort === -1 ? 'All' : rule.FromPort;
+          // Verificar quem está autorizado a se conectar
+          const source = rule.IpRanges.length > 0 ? rule.IpRanges[0].CidrIp : 'All';
+          // Avisar o usuário
+          message.reply(`Regra '${type}' encontrada na porta '${port}' para '${source}'`);
+        }
+      }
+    }
+  }
+}
 
 // Logar o bot usando o token
 client.login('YOUR_BOT_TOKEN');
